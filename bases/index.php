0fc44f3886bf969c584beb6b6aa95565d2269921
Fagot Oleko <olefag@gmail.com>
2020-09-15 19:23:41 +0200

    Initialisation

---
+++ bases/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial_scale=1.0">
+    <title>Bases1</title>
+    <link href="css/style.css" rel="stylesheet">
+    <script type="text/javascript" src="js/main.js"></script>
+</head>
+<body> 
+
+
+
+    
+
+   
+</body>    
+</html>    
