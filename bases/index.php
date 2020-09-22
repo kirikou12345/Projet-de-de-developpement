fb0fc3f54fbbcd4aa72cf89ee29d32f99fd51bbf
Fagot Oleko <fagot.oleko12@gmail.com>
2020-09-22 18:41:26 +0200

    nouveau bouton

--- bases/index.php
+++ bases/index.php
@@ -3,15 +3,36 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial_scale=1.0">
     <title>Bases1</title>
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
     <script type="text/javascript" src="js/main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body> 
 
 
 
+<button type="button" class="btn btn-primary">Primary</button>
+
+<style type="text/css">
+.roundedImage{
+    overflow:hidden;
+    -webkit-border-radius:50px;
+    -moz-border-radius:50px;
+    border-radius:50px;
+    width:90px;
+    height:90px;
+}
+</style>
+<div class="roundedImage" style="background:url(images/compresser-image-1200x628.jpg) no-repeat 0px 0px;" >
+    &nbsp;
+</div>
     
 
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
    
 </body>    
 </html>    
